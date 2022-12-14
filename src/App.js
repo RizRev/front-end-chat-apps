698d2f5fd4224388dd3a6e83246fff2ab866c5dc
RizRev <rizkyrevanda@gmail.com>
2022-12-15 02:01:37 +0700

    menit 26

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.css'; 
+import { io } from 'socket.io-client';
+// import { useState } from 'react';
 
 function App() {
+  const [message,setMessage] = useState("")
+  const socket = io("http://localhost:4000")
+  const handleMessage = () => {
+    socket.emit('message',message)
+    setMessage("")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={message} name="message"
+      onChange={(e)=>setMessage(e.target.value)} />
+      <button onClick={handleMessage}>tes</button>
     </div>
   );
 }
