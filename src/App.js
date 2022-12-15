e68047f6b27b6de0bb9f6fd90ea29cf1c1c81cf1
RizRev <rizkyrevanda@gmail.com>
2022-12-15 22:42:19 +0700

    selesai mas ean

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'; 
 import { io } from 'socket.io-client';
 // import { useState } from 'react';
 
 function App() {
   const [message,setMessage] = useState("")
-  const socket = io("http://localhost:4000")
+  const [socket,setSocket] = useState(null)
+  const [messages,setMessages] = useState([])
+  
   const handleMessage = () => {
     socket.emit('message',message)
     setMessage("")
   }
+
+  useEffect(()=>{
+    const resultSocket = io("http://localhost:4000")
+    setSocket(resultSocket)
+  },[])
+
+  useEffect(()=>{
+    if(socket){
+      socket.on("messageBe",(data)=>{
+        setMessages((current)=>[...current,data])
+      })
+    }
+  },[socket])
+
   return (
     <div className="App">
+      <ul>
+        {messages.map((item,index)=>(
+        <li key={index+1}>{item.message}  - {item.date}</li>
+        ))}
+      </ul>
       <input type="text" value={message} name="message"
       onChange={(e)=>setMessage(e.target.value)} />
+      <br/>
       <button onClick={handleMessage}>tes</button>
     </div>
   );
